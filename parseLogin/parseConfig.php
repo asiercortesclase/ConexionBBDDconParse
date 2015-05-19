098f3f6a21c782f122102e9954275accb4af3442
acortesirontec <acortes@irontec.com>
2015-05-19 14:09:08 +0200

    anadido el login con Parse

---
+++ parseLogin/parseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+class parseConfig{
+
+    const APPID = 'WwkSZyH6YbBLWI0K8WkoNK7wLBaRDeFyvU1Qk1u1';
+    const MASTERKEY = 'uXq4quJk6fndF7D7TOSLoLUjF0bWvdkxYxkjBHO8';
+    const RESTKEY = 'jLarnaCPQ8oj3ef9hfzXr6IPiTtELs9Iwt29xFGY';
+    const PARSEURL = 'https://api.parse.com/1/';
+}
+
+?>
